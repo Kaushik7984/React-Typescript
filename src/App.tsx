46674a3a2de9518f580234342aa217dc3211fbc6
Kaushik7984 <kaushik7984@gmail.com>
2024-06-22 10:13:45 +0530

    adding remove todo function

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
-import NewTodo from './components/NewTodo';
-import Todos from './components/Todos';
-import Todo from './models/todo';
+import { useState } from "react";
+
+import NewTodo from "./components/NewTodo";
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
-  const todos = [new Todo('Learn React'), new Todo('learn Typescript')]
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const addTodoHandler = (todoText: string) =>{
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
 
-  }
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.id !== todoId)
+    })
+  };
 
   return (
     <div>
       <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
     </div>
   );
 }
